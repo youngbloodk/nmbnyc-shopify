fce0ea95a7b7ecbf54d561743ed827535fcd1f8c
kohlmanndean <61201957+kohlmanndean@users.noreply.github.com>
2022-03-18 20:32:59 -0700

    hello world

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -7,16 +7,13 @@
 const path = require('path')
 
 module.exports = {
-  theme: {
-    extend: {},
-    container: {
-      center: true,
-      padding: '1rem'
-    }
-  },
-  plugins: [],
-  content: [
-    path.resolve(__dirname, '**/*.{js,vue}'),
-    path.resolve(__dirname, '../shopify/**/*.liquid')
-  ]
+	theme: {
+		extend: {},
+		container: {
+			center: true,
+			padding: '1rem',
+		},
+	},
+	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms'), require('@tailwindcss/line-clamp'), require('@tailwindcss/aspect-ratio')],
+	content: [path.resolve(__dirname, '**/*.{js,vue}'), path.resolve(__dirname, '../shopify/**/*.liquid')],
 }
